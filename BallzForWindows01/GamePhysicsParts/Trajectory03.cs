29aa1309d21ced672b7c58905d0261be808e04c3
Alex Doggett <adoggett00@gmail.com>
2019-12-27 12:54:09 -0500

    commit before merging with workbranch

--- BallzForWindows01/GamePhysicsParts/Trajectory03.cs
+++ BallzForWindows01/GamePhysicsParts/Trajectory03.cs
@@ -19,10 +19,6 @@ namespace BallzForWindows01.GamePhysicsParts
         string clsName = "Trajectory03";
         string nameTag = "";
 
-        //PointD startPoint;
-        //PointD endPoint;
-        //PointD rightPos;
-
         XMarker2 startMarker;
         XMarker2 endMarker;
 
@@ -30,17 +26,12 @@ namespace BallzForWindows01.GamePhysicsParts
         double oppLen, hypLen, adjLen, anglePreRotation;
 
         bool north, south;
-        //bool endPointSet;
 
         public Trajectory03()
         {
-            //startPoint = new PointD();
-            //endPoint = new PointD();
-            //rightPos = new PointD();
             startMarker = new XMarker2();
             endMarker = new XMarker2();
             north = south = false;
-            //endPointSet = false;
 
             startMarker.DrawColor = Color.Black;
             endMarker.DrawColor = Color.Black;
@@ -86,13 +77,13 @@ namespace BallzForWindows01.GamePhysicsParts
         public bool InEndRect(PointD p) { return _InEndRect(p.X, p.Y); }
         public bool InEndRect(double px, double py) { return _InEndRect(px, py); }
 
-
-
         public void SetStartPoint(PointD p) { _SetStartPoint(p.X, p.Y); }
         public void SetStartPoint(double sx, double sy) { _SetStartPoint(sx, sy); }
         public void SetEndPoint(PointD p) { _SetEndPoint(p.X, p.Y); }
         public void SetEndPoint(double ex, double ey) { _SetEndPoint(ex, ey); }
 
+
+        private bool _InEndRect(double px, double py) { return endMarker.InClickRect(px, py); }
 
         void _SetStartPoint(double sx, double sy)
         {
@@ -103,12 +94,8 @@ namespace BallzForWindows01.GamePhysicsParts
         {
             endMarker.Place(ex, ey);
 
-            // Temporary PointDs for calculating side lengths and rotation
-            PointD startPoint = new PointD(startMarker.Position);
-            PointD endPoint = new PointD(endMarker.Position);
-
-            SetSideLengths(startPoint, endPoint);
-            SetRotation(startPoint, endPoint);
+            SetSideLengths(startMarker.Position, endMarker.Position);
+            SetRotation(startMarker.Position, endMarker.Position);
         }
         void SetSideLengths(PointD startPoint, PointD endPoint)
         {
@@ -118,9 +105,6 @@ namespace BallzForWindows01.GamePhysicsParts
             adjLen = startPoint.DistanceTo(rightPoint);
         }
 
-
-
-        /// ----- SetRotation -----
 
         void SetRotation(PointD startPoint, PointD endPoint)
         {
@@ -146,68 +130,9 @@ namespace BallzForWindows01.GamePhysicsParts
             if (endPoint.X < startPoint.X && south) { rotation = Math.PI - anglePreRotation; return; }
         }
 
-        /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        #region SetRotation previous versions, rebuilt 2019-12-27    
-        //void SetRotation()
-        //{
-        //    PointD endPoint = new PointD();
-        //    PointD startPoint = new PointD();
-        //    startPoint.Set(startMarker.Position);
-        //    endPoint.Set(endMarker.Position);
-
-        //    north = south = false;
-        //    if (endPoint.Y < startPoint.Y) { north = true; }
-        //    if (endPoint.Y > startPoint.Y) { south = true; }
-        //    anglePreRotation = Math.Asin(oppLen / hypLen);
-        //    // north
-        //    if (endPoint.X == startPoint.X && north) { rotation = (3 * Math.PI) / 2; return; }
-        //    // south
-        //    if (endPoint.X == startPoint.X && south) { rotation = (Math.PI) / 2; return; }
-        //    // east
-        //    if (endPoint.X > startPoint.X && endPoint.Y == startPoint.Y) { rotation = 0; return; }
-        //    // west
-        //    if (endPoint.X < startPoint.X && endPoint.Y == startPoint.Y) { rotation = Math.PI; return; }
-        //    // northwest
-        //    if (endPoint.X > startPoint.X && north) { rotation = Math.PI * 2 - anglePreRotation; return; }
-        //    //northeast
-        //    if (endPoint.X < startPoint.X && north) { rotation = Math.PI + anglePreRotation; return; }
-        //    // southwest
-        //    if (endPoint.X > startPoint.X && south) { rotation = anglePreRotation; return; }
-        //    // southeast
-        //    if (endPoint.X < startPoint.X && south) { rotation = Math.PI - anglePreRotation; return; }
-        //}
-        /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        //void SetRotation()
-        //{
-        //    north = south = false;
-        //    if (endPoint.Y < startPoint.Y) { north = true; }
-        //    if (endPoint.Y > startPoint.Y) { south = true; }
-        //    anglePreRotation = Math.Asin(oppLen / hypLen);
-        //    // north
-        //    if (endPoint.X == startPoint.X && north) { rotation = (3 * Math.PI) / 2; return; }
-        //    // south
-        //    if (endPoint.X == startPoint.X && south) { rotation = (Math.PI) / 2; return; }
-        //    // east
-        //    if (endPoint.X > startPoint.X && endPoint.Y == startPoint.Y) { rotation = 0; return; }
-        //    // west
-        //    if (endPoint.X < startPoint.X && endPoint.Y == startPoint.Y) { rotation = Math.PI; return; }
-        //    // northwest
-        //    if (endPoint.X > startPoint.X && north) { rotation = Math.PI * 2 - anglePreRotation; return; }
-        //    //northeast
-        //    if (endPoint.X < startPoint.X && north) { rotation = Math.PI + anglePreRotation; return; }
-        //    // southwest
-        //    if (endPoint.X > startPoint.X && south) { rotation = anglePreRotation; return; }
-        //    // southeast
-        //    if (endPoint.X < startPoint.X && south) { rotation = Math.PI - anglePreRotation; return; }
-        //}
-
-        #endregion SetRotation previous versions, rebuilt 2019-12-27
-        /// ----- SetRotation -----
 
 
-
-        private bool _InEndRect(double px, double py) { return endMarker.InClickRect(px, py); }
+        
 
         void DrawPointMarkers(Graphics g)
         {
